021ccf78fd1533063b76496037773e19f9b2ff41
Paras Arora <paras@infostride.com>
2024-08-27 11:25:19 +0530

    text field mutable state

--- app/src/main/java/com/example/sample_2/MainActivity.kt
+++ app/src/main/java/com/example/sample_2/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.sample_2
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
-
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sample_2.ui.theme.Sample_2Theme
@@ -52,34 +57,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    
-   Column(
-       modifier.fillMaxSize().padding(30.dp)
-   ) {
 
-       Box(modifier = modifier.size(30.dp),)
-       Text(
-           text = "Hello $name!",
-//        color = Color.Blue,
-           fontStyle = FontStyle.Italic,
-           style = TextStyle(
-               fontSize = 30.sp,
-               color = Color.Green
-           ),
-           modifier = modifier
-       )
-     Box(modifier = modifier.size(30.dp),
+    val obj = remember {
+        mutableStateOf("Default string");
+    };
 
-         )
+    TextField(
+        value = obj.value,
+        onValueChange = {
+            obj.value = it;
+        },
+        enabled = true,
 
+    )
 
-       Image(
-           painter = painterResource(id = R.drawable.gas),
-           contentDescription = "Dummy image",
-           )
-   }
 }
-
 
 
 @Preview(
