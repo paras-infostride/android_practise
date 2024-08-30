61783271b5d3e2a7f1dc7452c441aba82aa305f8
Paras Arora <paras@infostride.com>
2024-08-30 12:19:59 +0530

    state hosting demo

--- app/src/main/java/com/example/sample_2/MainActivity.kt
+++ app/src/main/java/com/example/sample_2/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.sample_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,14 +12,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,14 +39,20 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(
-                            top = 40.dp,
-                            start = 40.dp
-                        ),
+                        .fillMaxSize().padding(20.dp),
                     color = MaterialTheme.colorScheme.background,
                     content = {
-                        Greeting("Android")
+                        var state : MutableState<Int> = remember {
+                            mutableStateOf(0)
+                        }
+                      Column {
+                          Greeting("Android" , modifier =  Modifier , state = state.value)
+                          SecondCompose(state.value,
+                              {
+                                  state.value++;
+                              }
+                          )
+                      }
                     }
                 )
             }
@@ -52,18 +62,18 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    
+fun Greeting(name: String, modifier: Modifier , state : Int) {
+
    Column(
-      modifier =  modifier.fillMaxSize().padding(30.dp),
+      modifier = Modifier
+          .size(height = 200.dp , width = 300.dp)
+          .padding(0.dp),
        verticalArrangement = Arrangement.SpaceBetween,
        horizontalAlignment = Alignment.CenterHorizontally
-
    ) {
 
-       Box(modifier = modifier.size(30.dp),)
        Text(
-           text = "Hello $name!",
+           text = "Hello $name! $state times",
 //        color = Color.Blue,
            fontStyle = FontStyle.Italic,
            style = TextStyle(
@@ -72,18 +82,21 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
            ),
            modifier = modifier
        )
-//     Box(modifier = modifier.size(30.dp),
-//
-//         )
 
 
-       Image(
-           painter = painterResource(id = R.drawable.gas),
-           contentDescription = "Dummy image",
-           )
+
    }
 }
 
+
+@Composable
+fun SecondCompose(state : Int , increment :   () -> Unit ) {
+    Button(
+        modifier = Modifier.padding(20.dp), onClick = { increment(); }) {
+        Text(text = "Click me ");
+
+    }
+}
 
 
 @Preview(
@@ -92,9 +105,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     heightDp = 600,
     showBackground = true
 )
+
 @Composable
 fun seePreview() {
     Sample_2Theme {
-        Greeting("Android")
+        Greeting("Android" , modifier =  Modifier.fillMaxSize() ,state =7)
     }
 }
